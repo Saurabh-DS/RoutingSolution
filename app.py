095d44929705119abf57b66a332068002ecad3d4
Saurabh Kabadi <68393690+Saurabh-DS@users.noreply.github.com>
2021-01-05 22:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from ortools.constraint_solver import pywrapcp
 
 app = Flask(__name__)
 
-
 # data = {}
 # data['distance_matrix'] = []
 # data['vehicle_capacities'] = []
@@ -19,15 +18,21 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def create_data_model():
+    int_features = [[x] for x in request.form.values()]
     data = {}
 #     data['distance_matrix'] = []
 #     data['vehicle_capacities'] = []
 #     data['demands'] = []
-    data['distance_matrix'] = request.values.get('distance_matrix')
-    data['vehicle_capacities'] = request.values.get('vehicle_capacities')
-    data['demands'] = request.values.get('demands')
-    data['num_vehicles'] = request.values.get('num_vehicles')
-    data['depot'] = request.values.get('depot')
+#     data['distance_matrix'] = request.values.get('distance_matrix')
+#     data['vehicle_capacities'] = request.values.get('vehicle_capacities')
+#     data['demands'] = request.values.get('demands')
+#     data['num_vehicles'] = request.values.get('num_vehicles')
+#     data['depot'] = request.values.get('depot')
+    data['distance_matrix'] = int_features[0]
+    data['vehicle_capacities'] = int_features[1]
+    data['demands'] = int_features[2]
+    data['num_vehicles'] = int_features[3]
+    data['depot'] = int_features[4]
     return data
 
 def print_solution(data, manager, routing, solution):
