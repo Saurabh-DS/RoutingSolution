3496cf39f8ce77be7c7a9ea52d1453334b666af0
Saurabh Kabadi <68393690+Saurabh-DS@users.noreply.github.com>
2021-01-06 10:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,10 @@ def home():
 
 
 @app.route('/predict',methods=['POST'])
-def create_data_model():
+def prediction():
+    def create_data_model():
 #     result=request.form
-    data = {}
+        data = {}
 #     data = {}
 #     data['distance_matrix'] = result['distance_matrix']
 #     data['vehicle_capacities'] = result['vehicle_capacities']
@@ -29,113 +30,113 @@ def create_data_model():
 #     data['distance_matrix'] = []
 #     data['vehicle_capacities'] = []
 #     data['demands'] = []
-    data['distance_matrix'] = [request.values.get('distance_matrix')]
-    data['vehicle_capacities'] = [request.values.get('vehicle_capacities')]
-    data['demands'] = [request.values.get('demands')]
-    data['num_vehicles'] = [request.values.get('num_vehicles')]
-    data['depot'] = [request.values.get('depot')]
+        data['distance_matrix'] = [request.values.get('distance_matrix')]
+        data['vehicle_capacities'] = [request.values.get('vehicle_capacities')]
+        data['demands'] = [request.values.get('demands')]
+        data['num_vehicles'] = [request.values.get('num_vehicles')]
+        data['depot'] = [request.values.get('depot')]
 #     data['distance_matrix'] = int_features[0]
 #     data['vehicle_capacities'] = int_features[1]
 #     data['demands'] = int_features[2]
 #     data['num_vehicles'] = int_features[3]
 #     data['depot'] = int_features[4]
-    return data
+        return data
 
-def print_solution(data, manager, routing, solution):
-    total_distance = 0
-    total_load = 0
-    for vehicle_id in range(data['num_vehicles']):
-        index = routing.Start(vehicle_id)
-        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
-        route_distance = 0
-        route_load = 0
-        while not routing.IsEnd(index):
-            node_index = manager.IndexToNode(index)
-            route_load += data['demands'][node_index]
-            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
-            previous_index = index
-            index = solution.Value(routing.NextVar(index))
-            route_distance += routing.GetArcCostForVehicle(
-                previous_index, index, vehicle_id)
-        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
-                                                 route_load)
-        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
-        plan_output += 'Load of the route: {}\n'.format(route_load)
-        print(plan_output)
-        total_distance += route_distance
-        total_load += route_load
-    print('Total distance of all routes: {}m'.format(total_distance))
-    print('Total load of all routes: {}'.format(total_load))
-
-
-def main():
-    """Solve the CVRP problem."""
-    # Instantiate the data problem.
-    data = create_data_model()
-
-    # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                           data['num_vehicles'], data['depot'])
-
-    # Create Routing Model.
-    routing = pywrapcp.RoutingModel(manager)
+    def print_solution(data, manager, routing, solution):
+        total_distance = 0
+        total_load = 0
+        for vehicle_id in range(data['num_vehicles']):
+            index = routing.Start(vehicle_id)
+            plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+            route_distance = 0
+            route_load = 0
+            while not routing.IsEnd(index):
+                node_index = manager.IndexToNode(index)
+                route_load += data['demands'][node_index]
+                plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
+                previous_index = index
+                index = solution.Value(routing.NextVar(index))
+                route_distance += routing.GetArcCostForVehicle(
+                    previous_index, index, vehicle_id)
+            plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
+                                                     route_load)
+            plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+            plan_output += 'Load of the route: {}\n'.format(route_load)
+            print(plan_output)
+            total_distance += route_distance
+            total_load += route_load
+        print('Total distance of all routes: {}m'.format(total_distance))
+        print('Total load of all routes: {}'.format(total_load))
 
 
-    # Create and register a transit callback.
-    def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return data['distance_matrix'][from_node][to_node]
+    def main():
+        """Solve the CVRP problem."""
+        # Instantiate the data problem.
+        data = create_data_model()
 
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+        # Create the routing index manager.
+        manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                               data['num_vehicles'], data['depot'])
 
-    # Define cost of each arc.
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-
-    # Add Capacity constraint.
-    def demand_callback(from_index):
-        """Returns the demand of the node."""
-        # Convert from routing variable Index to demands NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        return data['demands'][from_node]
-
-    demand_callback_index = routing.RegisterUnaryTransitCallback(
-        demand_callback)
-    routing.AddDimensionWithVehicleCapacity(
-        demand_callback_index,
-        0,  # null capacity slack
-        data['vehicle_capacities'],  # vehicle maximum capacities
-        True,  # start cumul to zero
-        'Capacity')
-
-    # Setting first solution heuristic.
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-    search_parameters.local_search_metaheuristic = (
-        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
-    search_parameters.time_limit.FromSeconds(1)
+        # Create Routing Model.
+        routing = pywrapcp.RoutingModel(manager)
 
 
-# @app.route('/predict_api',methods=['POST'])
-# def predict_api():
-    solution = routing.SolveWithParameters(search_parameters)
-#     return render_template('OptimalRoute.html', prediction_text=solution)
-    return solution
-    # # Print solution on console.
-    if solution:
-        print_solution(data, manager, routing, solution)
-#         return render_template('OptimalRoute.html', prediction_text=print_solution(data, manager, routing, solution))
+        # Create and register a transit callback.
+        def distance_callback(from_index, to_index):
+            """Returns the distance between the two nodes."""
+            # Convert from routing variable Index to distance matrix NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return data['distance_matrix'][from_node][to_node]
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    solution = routing.SolveWithParameters(search_parameters)
-    if solution:
-        print_solution(data, manager, routing, solution)
-    return jsonify()
+        transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+        # Define cost of each arc.
+        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+
+        # Add Capacity constraint.
+        def demand_callback(from_index):
+            """Returns the demand of the node."""
+            # Convert from routing variable Index to demands NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            return data['demands'][from_node]
+
+        demand_callback_index = routing.RegisterUnaryTransitCallback(
+            demand_callback)
+        routing.AddDimensionWithVehicleCapacity(
+            demand_callback_index,
+            0,  # null capacity slack
+            data['vehicle_capacities'],  # vehicle maximum capacities
+            True,  # start cumul to zero
+            'Capacity')
+
+        # Setting first solution heuristic.
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        search_parameters.local_search_metaheuristic = (
+            routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+        search_parameters.time_limit.FromSeconds(1)
+
+
+    # @app.route('/predict_api',methods=['POST'])
+    # def predict_api():
+        solution = routing.SolveWithParameters(search_parameters)
+    #     return render_template('OptimalRoute.html', prediction_text=solution)
+#         return solution
+        # # Print solution on console.
+        if solution:
+#             print_solution(data, manager, routing, solution)
+            return render_template('OptimalRoute.html', prediction_text=print_solution(data, manager, routing, solution))
+
+#     @app.route('/predict_api',methods=['POST'])
+#     def predict_api():
+#         solution = routing.SolveWithParameters(search_parameters)
+#         if solution:
+#             print_solution(data, manager, routing, solution)
+#         return jsonify()
 
 if __name__ == "__main__":
     app.run(debug=True)
