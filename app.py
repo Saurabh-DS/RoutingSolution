d194ec633cbb4255d0902cc952a90331850d9918
Saurabh Kabadi <68393690+Saurabh-DS@users.noreply.github.com>
2021-01-12 13:06:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def home():
     return render_template('OptimalRoute.html')
 
-@app.route('/main', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 
 def outs():
     def create_data_model():
